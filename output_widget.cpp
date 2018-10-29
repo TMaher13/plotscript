dc1f75f31604dbc9c982ba0733d33a9c39e33ae9
TMaher13 <tomm13@vt.edu>
2018-10-29 00:24:22 -0400

    Fixed error handling

--- output_widget.cpp
+++ output_widget.cpp
@@ -54,17 +54,24 @@ void OutputWidget::getResult(std::string result) {
 
 void OutputWidget::getPoint(Expression exp) {
 
-  int x, y = 0;
-  double diameter = 0.0;
+  int x = 0;
+  int y = 0;
+  int diameter = 0;
   QPen pen(Qt::black);
   x = exp.getTail().at(0).head().asNumber();
   y = exp.getTail().at(1).head().asNumber();
 
+  if()
+
   if(exp.property_list.find("\"size\"") != exp.property_list.end()) {
     diameter = exp.get_property("\"size\"").head().asNumber();
 
-    if(diameter < 0.0) {
-      getError("Error in point size: negative number given.");
+    if(!exp.get_property("\"size\"").head()isNumber())
+    getError("Error in point size: size is not a Number.");
+    return;
+
+    if(diameter < 0) {
+      getError("Error in point size: negative Number given.");
       return;
     }
 
@@ -79,7 +86,10 @@ void OutputWidget::getPoint(Expression exp) {
 
 
 void OutputWidget::getLine(Expression exp) {
-  int x1, x2, y1, y2 = 0;
+  int x1 = 0;
+  int x2 = 0;
+  int y1 = 0;
+  int y2 = 0;
   int width = 0;
 
   if(exp.property_list.find("\"thickness\"") != exp.property_list.end())
@@ -102,8 +112,20 @@ void OutputWidget::getLine(Expression exp) {
 
 void OutputWidget::getText(Expression exp) {
 
-  int x, y = 0;
+  int x = 0;
+  int y = 0;
   if(exp.property_list.find("\"position\"") != exp.property_list.end()) {
+    std::cout << "Uhh " << exp.get_property("\"position\"").getTail().size() << "Yup\n";;
+    if(exp.get_property("\"position\"").getTail().size() != 2) {
+      emit getError("Error in call to make-text: invalid argument");
+      return;
+    }
+
+    if(exp.get_property("\"position\"").get_property("\"object-name\"").head().asString() != "\"point\"") {
+      emit getError("Error in call to make-text: postion is not a make-point object");
+      return;
+    }
+
     Expression position = exp.get_property("\"position\"");
     x = position.getTail().at(0).head().asNumber();
     y = position.getTail().at(1).head().asNumber();
