8d756f520b58872288f48b88ab98f4f51be39465
TMaher13 <tomm13@vt.edu>
2018-11-30 15:22:46 -0500

    Task 3 %start, %stop, and %reset for TUI

--- interpreter_thread.hpp
+++ interpreter_thread.hpp
@@ -16,49 +16,51 @@ private:
   ThreadSafeQueue<std::string>* inputQueuePtr;
   ThreadSafeQueue<output_type>* outputQueuePtr;
 
+  //bool forceStop;
+
 public:
+
+  void killThread(); // {
 
   InterpreterThread(ThreadSafeQueue<std::string>* input_queue_ptr, ThreadSafeQueue<output_type>* output_queue_ptr, Interpreter& interpreter) {
     inputQueuePtr = input_queue_ptr;
     outputQueuePtr = output_queue_ptr;
     interp = interpreter;
-    std::cout << "Interpreter thread created.\n";
+    //forceStop = false;
+    //std::cout << "Interpreter thread created.\n";
   };
 
   // Event loop for interpreter thread
   void operator()() {
-    //int i = 0;
     //std::cout << "In Interpreter thread.\n";
     while(1) {
       std::string m;
-      //std::cout << i << '\n';
       if(inputQueuePtr->try_pop(m)) {
+
+        if(m == "%stop" || m == "%reset")
+          break;
+          
         std::istringstream expression(m);
         output_type toSend;
 
         if(!interp.parseStream(expression)) {
-          //std::cout << i << '\n';
           toSend.isError = true;
           toSend.err_result = SemanticError(std::string("Error: Invalid Expression. Could not parse."));
           outputQueuePtr->push(toSend);
-          //std::cout << i << '\n';
           // transport in the output queue that you sent an expression or an error
         }
         else{
           try{
-            //std::cout << i << i << '\n';
             Expression exp = interp.evaluate();
-            //std::cout << exp << std::endl;
             toSend.isError = false;
             toSend.exp_result = exp;
             outputQueuePtr->push(toSend);
-            //std::cout << i << i << '\n';
+
           }
           catch(const SemanticError & ex) {
             toSend.isError = true;
             toSend.err_result = SemanticError(ex.what());
             outputQueuePtr->push(toSend);
-            //std::cerr << ex.what() << std::endl;
           }
         }
 
